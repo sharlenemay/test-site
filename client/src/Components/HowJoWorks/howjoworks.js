89cb47276cb5132692c8aa35a0d3e8a3d8fbb2b6
Myka Kugaya <mykakug129@gmail.com>
2020-10-25 14:43:32 -0700

    demo finished

--- client/src/Components/HowJoWorks/howjoworks.js
+++ client/src/Components/HowJoWorks/howjoworks.js
@@ -1,12 +1,21 @@
-import React from "react";
-import { Container, Row, Col } from "react-bootstrap";
+import React, {useState} from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "./howjoworks.css";
 import ExtensionImg from "../../Jo/extension-img.svg";
+import ModalVideo from 'react-modal-video';
 
 export default function HowJoWorks() {
+  const [isOpen, setOpen] = useState(false)
   return (
     <Container className="howjoworks">
       <Row>
+        <Col className="addPadding" id="extensionColumn">
+          <img
+            className="extensionimg"
+            src={ExtensionImg}
+            alt="google chrome extension"
+          />
+        </Col>
         <Col className="addPadding" xs={12} sm={12} md={6} lg={6}>
           <h2 className="howjoworksheading">How Jo Works</h2>
           <p>
@@ -20,13 +29,10 @@ export default function HowJoWorks() {
             out for, and Jo will react accordingly when you are browsing
             different websites.
           </p>
-        </Col>
-        <Col className="addPadding" id="extensionColumn">
-          <img
-            className="extensionimg"
-            src={ExtensionImg}
-            alt="google chrome extension"
-          />
+          <>
+          <ModalVideo channel='youtube' isOpen={isOpen} autoplay videoId="TpL9aSqB-Ao" onClose={() => setOpen(false)} />
+          <Button variant="primary" onClick={() => setOpen(true)} id="demoBtn">Watch Demo</Button>
+          </>
         </Col>
       </Row>
     </Container>
